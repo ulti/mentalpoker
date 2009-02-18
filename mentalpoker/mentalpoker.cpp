4a1fb65120aa5930c17a92653f57318c0da1470b
Matt Reynolds <matt.reynolds@gmail.com>
2009-02-17 23:06:16 -0800

    added code to generate keys for one party

--- mentalpoker/mentalpoker.cpp
+++ mentalpoker/mentalpoker.cpp
@@ -1,18 +1,34 @@
-#include <tchar.h>
 #include <cstdio>
 #include <cstdlib>
 #include <time.h>
+#ifdef _MSC_VER
+# include <tchar.h>
+#endif
 
 #include <openssl/bn.h>
 
-void bn_print(BIGNUM *n)
+#define BITS 32
+
+void bn_print(const BIGNUM *a)
 {
-	int len = BN_num_bytes(n);
-	unsigned char *data = (unsigned char*)malloc(len);
-	BN_bn2bin(n, data);
-	for(int i = 0; i < len; i++)
-		printf("%02x", data[i]);
-	free(data);
+	int i,j,v,z=0;
+
+	if (a->neg)
+		printf("-");
+	if (BN_is_zero(a))
+		printf("0");
+
+	for (i=a->top-1; i >=0; i--) {
+		for (j=BN_BITS2-4; j >= 0; j-=4) {
+			/* strip leading zeros */
+			v=((int)(a->d[i]>>(long)j))&0x0f;
+			if (z || (v != 0)) {
+				printf("%x", v);
+				z=1;
+			}
+		}
+	}
+	return;
 }
 
 BIGNUM *bn_gen_prime()
@@ -20,25 +36,73 @@ BIGNUM *bn_gen_prime()
 	BIGNUM *ret = BN_new();
 	if(ret == NULL)
 		return NULL;
-	if(BN_generate_prime_ex(ret, 32, true, NULL, NULL, NULL) != 1) {
+	if(BN_generate_prime_ex(ret, BITS, true, NULL, NULL, NULL) != 1) {
 		BN_free(ret);
 		ret = NULL;
 	}
 	return ret;
 }
 
+#ifdef _MSC_VER
 int _tmain(int argc, _TCHAR* argv[])
+#else
+int main(int argc, char *argv[])
+#endif
 {
-	BIGNUM *bn;
+	BIGNUM *rangemax,*gcd,*p,*q,*n,*phi,*e1,*e2,*d1,*d2;
+	BN_CTX *cntx = BN_CTX_new();
 
 	srand((unsigned int)time(NULL));
-	bn = bn_gen_prime();
-	bn_print(bn);
-	printf("\n");
 
-	BN_free(bn);
+	rangemax = BN_new();
+	BN_set_word(rangemax, 1<<(BITS-1));
+
+	p = bn_gen_prime();
+	q = bn_gen_prime();
+
+	// n = p*q
+	n = BN_new();
+	BN_mul(n,p,q,cntx);
+
+	// phi = (p-1)(q-1)
+	phi = BN_new();
+	BN_mul(phi,p,q,cntx);
+	BN_sub(phi,phi,p);
+	BN_sub(phi,phi,q);
+	BN_add(phi,phi,BN_value_one());
+
+	// choose e so it is relatively prime to phi
+	e1 = BN_new();
+	gcd = BN_new();
+	while(1) {
+		BN_rand_range(e1,rangemax);
+		BN_gcd(gcd,e1,phi,cntx);
+		if(BN_is_one(gcd))
+			break;
+	}
+	BN_free(gcd);
+
+	// d = e^(-1) mod phi
+	d1 = BN_new();
+	BN_mod_inverse(d1,e1,phi,cntx);
+
+	printf("p: "); bn_print(p); printf("\n");
+	printf("q: "); bn_print(q); printf("\n");
+	printf("phi: "); bn_print(phi); printf("\n");
+	printf("n: "); bn_print(n); printf("\n");
+	printf("e1: "); bn_print(e1); printf("\n");
+	printf("d1: "); bn_print(d1); printf("\n");
+
+	BN_free(rangemax);
+	BN_free(p);
+	BN_free(q);
+	BN_free(phi);
+	BN_free(n);
+	BN_free(e1);
+	BN_free(d1);
+	BN_CTX_free(cntx);
+
 	printf("Press Enter to continue\n");
 	getchar();
 	return 0;
 }
-
